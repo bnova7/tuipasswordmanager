b9b34fc482aea40a2be696eaa20d9474a38c4459
bluenovaeng <cemartin216@gmail.com>
2026-05-06 21:50:44 -0400

    updated password generator

--- passwordgenerator.py
+++ passwordgenerator.py
@@ -1,12 +1,23 @@
 import random 
 import string 
+import pyperclip
 
 
 
 
+try:
+    random = ''.join([random.choice(string.ascii_letters + string.digits + string.punctuation)for n in range(16) ]) 
 
+    try:
+        print(random)
+        if int(input("Enter 1 to copy password.")):
+            pyperclip.copy(random)
 
-random = ''.join([random.choice(string.ascii_letters + string.digits + string.punctuation)for n in range(16) ]) 
+    except ValueError:
+        print("Invalid input.")
+
+except KeyboardInterrupt:
+    print("\nGoodbye.")
 
 
 
